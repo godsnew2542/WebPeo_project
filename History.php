3f97166b273776ef4fbb88606abed6e9875729b9
godsnew2542 <godsnew2542@gmail.com>
2019-04-27 21:26:51 +0700

    เช็คเมื่อ row =0

--- History.php
+++ History.php
@@ -47,6 +47,8 @@
     if (!$result) {
       echo mysql_error() . '<br>';
       die('Can not access database!');
+    }else if(mysqli_num_rows($result) == 0){
+      echo "ไม่มีประวัติของคุณ";
     } else {
 
       while ($row = mysqli_fetch_assoc($result)) {
