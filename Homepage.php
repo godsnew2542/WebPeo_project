a97f59e5a8a04cb492dbb4e9f097c9f5a3c1948a
meipraewrung <38246225+meipraewrung@users.noreply.github.com>
2019-04-27 22:23:40 +0700

    Update Homepage.php

--- Homepage.php
+++ Homepage.php
@@ -2,20 +2,30 @@
 <html lang="en">
 
 <head>
-  <title>Title</title>
+  <title>Homepage</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <!-- time -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
-    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Itim|Kanit|PT+Sans|Prompt|Raleway|Slabo+27px|Taviraj" rel="stylesheet">
+  <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+  <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Itim|Kanit|PT+Sans|Prompt|Raleway|Slabo+27px|Taviraj" rel="stylesheet">
+
+  <style>
+    /*sidemenu ด้านซ้าย*/
+    .l-sidenav {
+        position: fixed; 
+        z-index: 1040; 
+        top: 0; 
+        left: 0;    
+        height: 100%; 
+        width: 0; 
+        overflow-x: hidden; 
+    }   
+  </style>
 </head>
 
 <body>
@@ -26,40 +36,32 @@
 
   if (isset($_SESSION['User'])) {
     ?>
-     <nav class="navbar navbar-inverse">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"><?php echo '&nbsp' . '&nbsp' . $_SESSION['User'] ?></a>
-        </div>
-        <div class="collapse navbar-collapse " id="myNavbar">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Homepage</a></li>
-            <li><a href="History.php">History</a></li>
-            <li><a href="logout.php?logout">Logout</a></li>
-          </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">
+        <?php echo "<font color=red><b>Welcome</b></font>" . '&nbsp' . '&nbsp' . $_SESSION['User'] ?></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav px-md-5 ml-auto">
+          <?php echo '<a href="#">Homepage</a>'; ?> &nbsp;&nbsp;&nbsp;&nbsp;
+          <?php echo '<a href="History.php">History</a>'; ?> &nbsp;&nbsp;&nbsp;&nbsp;
+          <?php echo '<a href="logout.php?logout">Logout</a>'; ?>
         </div>
       </div>
     </nav>
-    <form name="registration" action="flight.php" method="post">
+    <br> <br>
+    <form name="registration" action="Flight.php" method="post">
       <h1>Homepage</h1>
-      
-      <!-- ตกแต่ง -->
-      <div>
+
       Flight From <select name="Flight_From">
         <?php
-        $sql = 'SELECT distinct FlightFrom FROM flight order by FlightFrom asc';
+        $sql = 'SELECT FlightFrom FROM flight';
         $result = mysqli_query($connect, $sql);
         if (!$result) {
           echo mysql_error() . '<br>';
           die('Can not access database!');
         } else {
-          echo '<option value="" selected>----</option>';
-
           while ($row = mysqli_fetch_assoc($result)) {
             echo '<option value=';
             while (list($key, $value) = each($row)) {
@@ -71,20 +73,16 @@
           }
         }
         ?>
-        <br>
       </select>
-
-      <br><br>
+      <br>
       Flight To <select name="Flight_To">
         <?php
-        $sql = 'SELECT distinct FlightTo FROM flight order by FlightTo asc';
+        $sql = 'SELECT FlightTo FROM flight';
         $result_1 = mysqli_query($connect, $sql);
-
         if (!$result_1) {
           echo mysql_error() . '<br>';
           die('Can not access database!');
         } else {
-          echo '<option value="" selected>----</option>';
           while ($row = mysqli_fetch_assoc($result_1)) {
             echo '<option value=';
             while (list($key, $value) = each($row)) {
@@ -101,49 +99,54 @@
       <br><br>
 
 
-      <!-- จำนวนผู้โดยสาร <br> 
-      <?php //ห้ามเกิน9 คน ?>
-                          
-      ผู้ใหญ่ : <input name="Adult" type="text" placeholder="" required pattern="[\d0-9]{1}"> <br>
-      เด็ก : <input name="child" type="text" placeholder="" required pattern="[\d0-9]{1}"> <br>
-
-      วันออกเดินทาง
-      <input class="datepicker" width="200" placeholder="04/18/2019" required>
-      <script>
-        $('.datepicker').datepicker({
-          uiLibrary: 'bootstrap3'
-        });
-      </script> -->
-
-<!-- TGทำต่อ
-        ชั้นที่นั่ง
+      จำนวนผู้โดยสาร <br> 
+      ผู้ใหญ่ : <input name="Adult"  type="text" placeholder="5" required pattern="[\d0-9]{1}"> <br>
+      เด็ก : <input name="child"  type="text" placeholder="4" required pattern="[\d0-9]{1}"> <br><br>
+      <?php  
+      ?>
+      ชั้นที่นั่ง
       <select name="class">
         <option value="" selected>-------</option>
         <option value="economy">Economy</option>
         <option value="business">Business</option>
       </select>
+      <br><br>
+
+      <label for="RFname">ชื่อผู้จอง</label>
+      <input type="text" name="RFname">
       <br>
+      <label for="RFname">นามสกุลผู้จอง</label>
+      <input type="text" name="RLname">
+      <br><br>
 
-        <label for="RFname">ชื่อผู้จอง</label>
-       <input type="text" name="RFname">
-
-       <label for="RFname">นามสกุลผู้จอง</label>
-       <input type="text" name="RLname">
-       <br> -->
-
-      <button type="submit" name="Search">Search</button> <br> <br>
-
-    </div>
-    
+      วันออกเดินทาง
+      <input class="datepicker" width="200" placeholder="04/18/2019" required>
+      <script>
+        $('.datepicker').datepicker({
+          uiLibrary: 'bootstrap4'
+        });
+      </script><br> 
+      
+      <button type="submit" name="net">net</button> 
     </form>
-    
-
-
   <?php
 } else {
   header("location:login.php");
 }
 ?>
+
+
+<script src="https://unpkg.com/jquery@3.3.1/dist/jquery.min.js"></script>
+<script src="https://unpkg.com/bootstrap@4.1.0/dist/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+$(function(){
+    /*เมื่อปุ่มปิดเปิด เมนูด้านซ้ายถูกคลิก*/
+    $(".close-l-sidenav,.open-l-sidenav").on("click",function(){
+        var toggleWidth = ($(".l-sidenav").width()==0)?250:0;
+        $(".l-sidenav").width(toggleWidth);
+    });
+});
+</script>
 
 </body>
 
