72c24ed2ef1e5e85792d2c4e17cdb70e40bf3972
meipraewrung <38246225+meipraewrung@users.noreply.github.com>
2019-04-28 20:59:06 +0700

    Update InsertInformation.php

--- InsertInformation.php
+++ InsertInformation.php
@@ -9,18 +9,11 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <!---navbar--->
-  <script src="https://unpkg.com/jquery@3.3.1/dist/jquery.min.js"></script>
-  <script src="https://unpkg.com/bootstrap@4.1.0/dist/js/bootstrap.min.js"></script>
-  <!---navbar button--->
-  <script type="text/javascript">
-    $(function(){
-      $(".close-l-sidenav,.open-l-sidenav").on("click",function(){
-          var toggleWidth = ($(".l-sidenav").width()==0)?250:0;
-          $(".l-sidenav").width(toggleWidth);
-      });
-    });
-  </script>
+  <!---time--->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+  <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=Itim|Kanit|PT+Sans|Prompt|Raleway|Slabo+27px|Taviraj" rel="stylesheet">
   <!---ค่าคน--->
   <script language="javascript">
         function check() {
@@ -31,6 +24,21 @@
             }
         }
     </script>  
+  <style>
+    /*sidemenu ด้านซ้าย*/
+    .l-sidenav {
+        position: fixed; 
+        z-index: 1040; 
+        top: 0; 
+        left: 0;    
+        height: 100%; 
+        width: 0; 
+        overflow-x: hidden; 
+    }   
+    .text-center {
+        text-align: center;
+    }
+  </style>
 </head>
 <body>
   <?php
@@ -58,7 +66,7 @@
   <div class="row">
     <div class="col-md-3"></div>
     <div class="col-md-6">
-    <h1>Information</h1>
+    <h1>Information</h1><br>
       จำนวนผู้โดยสาร <br>
       ผู้ใหญ่  
       <select name="Adult">
@@ -93,15 +101,35 @@
       <label for="RFname">นามสกุลผู้จอง</label>
       <input type="text" name="RLname">
       <br><br>
+      วันเดินทาง
+      <input class="datepicker" width="200" placeholder="04/18/2019" required>
+      <script>
+        $('.datepicker').datepicker({
+          uiLibrary: 'bootstrap4'
+        });
+      </script>
+      <br>
       <button type="submit" name="net">next</button>
       </div>
       <div class="col-md-3"></div>
     </form>
-<?php
-} else {
-  header("location:login.php");
-}}
+  <?php
+  }else{
+    header("location:login.php");
+  }
+}
 ?>
-
+<!---navbar--->
+<script src="https://unpkg.com/jquery@3.3.1/dist/jquery.min.js"></script>
+<script src="https://unpkg.com/bootstrap@4.1.0/dist/js/bootstrap.min.js"></script>
+<!---navbar button--->
+<script type="text/javascript">
+  $(function(){
+    $(".close-l-sidenav,.open-l-sidenav").on("click",function(){
+        var toggleWidth = ($(".l-sidenav").width()==0)?250:0;
+        $(".l-sidenav").width(toggleWidth);
+     });
+  });
+</script>
 </body>
 </html>
