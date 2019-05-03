0f2dba6f4725c3c07de0d34b2054231de06c29b6
meipraewrung <38246225+meipraewrung@users.noreply.github.com>
2019-05-03 20:44:26 +0700

    Update flight.php

--- flight.php
+++ flight.php
@@ -143,6 +143,4 @@
     ?>
 
 </body>
-<!---Button BacktoTop--->
-<a style="display:scroll;position:fixed;bottom:5px;right:5px;" class="backtotop" href="#" rel="nofollow" title="Back to Top"><img style="border:0;" src="http://2.bp.blogspot.com/-fBSW--O5-eA/UIao-OcGSCI/AAAAAAAAEI8/-GomJZ4SCm4/s1600/uptop2.png"/></a>
 </html>
