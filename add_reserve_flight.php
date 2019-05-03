3b85633852cb72dc0d0287dca66c812224806f14
Lalita <ja.ea5923@gmail.com>
2019-05-03 21:11:56 +0700

    error

--- add_reserve_flight.php
+++ add_reserve_flight.php
@@ -5,82 +5,137 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Title Page</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.0/css/bulma.css"/>
+        <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+  <script language="javascript">
+        function check() {
+            if (!(document.login.email.value == "" &&
+                    document.login.password.value == "")) {
+                return confirm('คุณแน่ใจแล้วใช่ไหม');
+            }
+        }
+    </script>
     </head>
-<style>
-    /* Center the loader */
-#loader {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  z-index: 1;
-  width: 150px;
-  height: 150px;
-  margin: -75px 0 0 -75px;
-  border: 16px solid #f3f3f3;
-  border-radius: 50%;
-  border-top: 16px solid #3498db;
-  width: 120px;
-  height: 120px;
-  -webkit-animation: spin 1s linear infinite;
-  animation: spin 1s linear infinite;
+    <body>
+    <?php
+  require('db.php');
+  session_start();
+
+  if (isset($_SESSION['User'])) {
+    ?>
+     <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><?php echo '&nbsp' . '&nbsp' . $_SESSION['User'] ?></a>
+        </div>
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="Homepage.php">Homepage</a></li>
+            <li><a href="History.php">History</a></li>
+            <li><a href="logout.php?logout">Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <h1>thank you</h1>
+
+    
+<?php
+  require('db.php');
+  $sql = 'INSERT into reserve_flight 
+  VALUES ( "","'.$_POST['Adult'].'",
+              "'.$_POST['child'].'",
+              "'.$_POST['$price'].'",
+              "'.date('d-m-Y').'")';
+
+  $result = mysqli_query($connect, $sql);
+
+  $sql1 = 'INSERT into reserve_flight
+  VALUES     ("'.$_POST['User_ID'].'",
+              "'.$_POST['RFname'].'",
+              "'.$_POST['RLname'].'",
+              "'.$_POST['class'].'",
+               "'.$_POST['TrDate'].'" )';
+
+  $result1 = mysqli_query($connect, $sql1);            
+
+
+    $select = 'select*from reserve_flight';
+    $result2 = mysqli_query($connect, $select);
+
+    
+      echo '<table border="1" cellpading="0" cellspacing="0">';
+      echo '<tr>'.'<td>'.'RID'.'</td>';
+      echo '<td>'.'User_ID'.'</td>';
+      echo '<td>'.'RFname'.'</td>';
+      echo '<td>'.'RLname'.'</td>';
+      echo '<td>'.'Class'.'</td>';
+      echo '<td>'.'adult_total'.'</td>';
+      echo '<td>'.'teen_total'.'</td>';
+      echo '<td>'.'Price'.'</td>';
+      echo '<td>'.'Date_reserve'.'</td>';
+      echo '<td>'.'Trv_date'.'</td>'.'</tr>';
+    
+
+    if($_SESSION['class'] == "economy"){
+     
+      $_SESSION['class'] = "economy";
+
+    while($row = mysqli_fetch_assoc($result2)){
+      echo '<tr>'.'<td>'."".'</td>';
+      echo '<td>'.$row['User_ID'].'</td>';
+      echo '<td>'.$row['RFname'].'</td>';
+      echo '<td>'.$row['RLname'].'</td>';
+      echo '<td>'.'Economy'.'</td>';
+      echo '<td>'.$row['adult_total'].'</td>';
+      echo '<td>'.$row['teen_total'].'</td>';
+      
+      
+      echo '<td>'.$price.'</td>';
+      echo '<td>'.$row['Date_reserve'].'</td>';
+      echo '<td>'.$row['Trv_date'].'</td>'.'</tr>';
+    }
+
+  }else if($_SESSION['class'] == "business"){
+    while($row = mysqli_fetch_assoc($result2)){
+      echo '<tr>'.'<td>'."".'</td>';
+      echo '<td>'.$row['User_ID'].'</td>';
+      echo '<td>'.$row['RFname'].'</td>';
+      echo '<td>'.$row['RLname'].'</td>';
+      echo '<td>'.'Business_Price'.'</td>';
+      echo '<td>'.$row['adult_total'].'</td>';
+      echo '<td>'.$row['teen_total'].'</td>';
+
+
+        
+      echo '<td>'.$price.'</td>';
+      echo '<td>'.$row['Date_reserve'].'</td>';
+      echo '<td>'.$row['Trv_date'].'</td>';
+    }
+  }
+  echo '</table>';
+    
+      
+        mysqli_close($connect);
+        
+        
+    
+
+?>
+
+<?php
+      } else {
+
+  header("location:login.php");
 }
-
-@-webkit-keyframes spin {
-  0% { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
-}
-
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}
-
-/* Add animation to "page content" */
-.animate-bottom {
-  position: relative;
-  -webkit-animation-name: animatebottom;
-  -webkit-animation-duration: 1s;
-  animation-name: animatebottom;
-  animation-duration: 1s
-}
-
-@-webkit-keyframes animatebottom {
-  from { bottom:-100px; opacity:0 } 
-  to { bottom:0px; opacity:1 }
-}
-
-@keyframes animatebottom { 
-  from{ bottom:-100px; opacity:0 } 
-  to{ bottom:0; opacity:1 }
-}
-
-#myDiv {
-  display: none;
-  text-align: center;
-}
-</style>
-
-    <body onload="myFunction()" style="margin:0;">
-    <div id="loader"></div>
-
-<div style="display:none;" id="myDiv" class="animate-bottom">
-  <h2>Success!!</h2>
-  <p>Thank you for choosing Airplane Modelling!!!</p>
-</div>
-    <script>
-var myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
-</script>
-
-
-    </body>
+?>
+</body>
 </html>
